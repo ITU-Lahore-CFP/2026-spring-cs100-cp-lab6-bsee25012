a49ace5a3fc395e65fcfc731b3349eddb24f854f
somanaslam <bsee25012@itu.edu.pk>
2026-03-29 20:21:58 +0500

    commit - 1 done

--- functions.cpp
+++ functions.cpp
@@ -3,10 +3,23 @@
 
 int sumOfSquares(int n) 
 {
+	int sum = 0;
+	for (int i = 1; i <= n; i++) {
+		sum += i * i;
+	}
+	return sum;
 }
 
 int findFactorial(int n)
 {
+
+	int factorial = 1;
+	if (n <= 1) {
+		return factorial;
+	}
+	
+	factorial = n * findFactorial(n - 1);
+	return factorial;
 }
 
 
